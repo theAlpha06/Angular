b6b19ace214e2e2cf0bec4e3e30a6d36794ed3a5
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-05-09 19:34:49 +0530

    classes in ts

---
+++ TypeScript/Classes.js
@@ -0,0 +1,60 @@
+"use strict";
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _EmployeeInformation_id;
+// Prefer pascal case for naming classes
+class EmployeeInformation {
+    constructor(id, name, address) {
+        _EmployeeInformation_id.set(this, void 0);
+        this.address = address;
+        __classPrivateFieldSet(this, _EmployeeInformation_id, id, "f");
+        this.name = name;
+    }
+    ;
+    //getter
+    get empId() {
+        return __classPrivateFieldGet(this, _EmployeeInformation_id, "f");
+    }
+    //setter
+    set empId(id) {
+        __classPrivateFieldSet(this, _EmployeeInformation_id, id, "f");
+    }
+    getNameWithAddress() {
+        return `${this.name} stays at ${this.address}`;
+    }
+    static getEmployeeCount() {
+        return 50;
+    }
+}
+_EmployeeInformation_id = new WeakMap();
+// You can have either parametarized contructor or default contructor.
+let shubh = new EmployeeInformation(1, 'Shubham', 'Punjab');
+let address = shubh.getNameWithAddress();
+console.log(address);
+shubh.empId = 100;
+console.log(shubh.empId);
+// shubh.id; //!Error id is private
+// shubh.name; //!Error name is protected
+// let shubh = new EmployeeInformation();
+// shubh.id = 1;
+// shubh.name = 'Shubham';
+// shubh.address = 'Punjab';
+console.log(shubh);
+class Manager extends EmployeeInformation {
+    constructor(id, name, address) {
+        super(id, name, address);
+    }
+}
+let kriti = new Manager(2, 'Kriti', 'Punjab');
+console.log(kriti.getNameWithAddress());
+// Static members can be accessed with the class name
+console.log(EmployeeInformation.getEmployeeCount());
---
+++ TypeScript/Classes.ts
@@ -0,0 +1,66 @@
+// Prefer pascal case for naming classes
+class EmployeeInformation {
+
+    #id: number;
+    protected name: string;
+    address: string; // '!' is just to remove error
+
+    constructor(id: number, name: string, address: string){
+        this.address = address;
+        this.#id = id;
+        this.name = name;
+    };
+
+    //getter
+    get empId(): number {
+        return this.#id;
+    }
+
+    //setter
+    set empId(id: number) {
+        this.#id = id;
+    }
+
+    getNameWithAddress() : string {
+        return `${this.name} stays at ${this.address}`;
+    }
+
+    static getEmployeeCount():number {
+        return 50;
+    }
+}
+
+// You can have either parametarized contructor or default contructor.
+
+let shubh = new EmployeeInformation(1, 'Shubham', 'Punjab');
+let address = shubh.getNameWithAddress();
+console.log(address);
+
+shubh.empId = 100;
+console.log(shubh.empId);
+
+// shubh.id; //!Error id is private
+// shubh.name; //!Error name is protected
+
+// let shubh = new EmployeeInformation();
+// shubh.id = 1;
+// shubh.name = 'Shubham';
+// shubh.address = 'Punjab';
+
+console.log(shubh);
+
+class Manager extends EmployeeInformation{
+    constructor(id: number, name: string, address: string) {
+        super(id, name, address);
+    }
+
+    // getNameWithAddress(): string {
+    //     return `${this.name} is a manager at ${this.address}`;
+    // }
+}
+
+let kriti = new Manager(2, 'Kriti', 'Punjab');
+console.log(kriti.getNameWithAddress());
+
+// Static members can be accessed with the class name
+console.log(EmployeeInformation.getEmployeeCount());
