7327c1b410fb6511fe9a78ad891ffc72220cb13f
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-05-09 23:48:37 +0530

    Basic of Angular
    File structure and directives

--- hotelinventory/src/app/app.component.ts
+++ hotelinventory/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hotelinventory-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  //inline template
+  // template: `<h1>Hello World from inline template</h1>`,
+  // styles: [`
+  //   h1 {
+  //     color: red;
+  //   }
+  // `]
 })
 export class AppComponent {
   title = 'hotelinventory';
+
+  role = 'Admin';
 }
