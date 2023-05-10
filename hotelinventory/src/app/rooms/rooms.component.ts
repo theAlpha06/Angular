9d3e78832c906ed3349fcf708df64bfda711cdac
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-05-10 12:48:58 +0530

    Life cycle hooks

--- hotelinventory/src/app/rooms/rooms.component.ts
+++ hotelinventory/src/app/rooms/rooms.component.ts
@@ -17,35 +17,42 @@ export class RoomsComponent {
   }
   hideRooms = false;
 
-  roomList: RoomList[] = [
-    {
-      roomNumber: 101,
-      roomType: 'Deluxe',
-      amenities: ['TV', 'AC', 'Wifi'],
-      price: 2000,
-      photos: 'https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-      checkinTime: new Date('2020-01-01T10:00:00'),
-      checkoutTime: new Date('2020-01-01T12:00:00')
-    },
-    {
-      roomNumber: 102,
-      roomType: 'Standard',
-      amenities: ['TV', 'AC'],
-      price: 1500,
-      photos: 'https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-      checkinTime: new Date('2020-01-01T10:00:00'),
-      checkoutTime: new Date('2020-01-01T12:00:00'),
-    },
-    {
-      roomNumber: 103,
-      roomType: 'Economy',
-      amenities: ['TV'],
-      price: 1000,
-      photos: 'https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-      checkinTime: new Date('2020-01-01T10:00:00'),
-      checkoutTime: new Date('2020-01-01T12:00:00'),
-    }
-  ];
+  roomList: RoomList[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.roomList = [
+      {
+        roomNumber: 101,
+        roomType: 'Deluxe',
+        amenities: ['TV', 'AC', 'Wifi'],
+        price: 2000,
+        photos: 'https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+        checkinTime: new Date('2020-01-01T10:00:00'),
+        checkoutTime: new Date('2020-01-01T12:00:00')
+      },
+      {
+        roomNumber: 102,
+        roomType: 'Standard',
+        amenities: ['TV', 'AC'],
+        price: 1500,
+        photos: 'https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+        checkinTime: new Date('2020-01-01T10:00:00'),
+        checkoutTime: new Date('2020-01-01T12:00:00'),
+      },
+      {
+        roomNumber: 103,
+        roomType: 'Economy',
+        amenities: ['TV'],
+        price: 1000,
+        photos: 'https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+        checkinTime: new Date('2020-01-01T10:00:00'),
+        checkoutTime: new Date('2020-01-01T12:00:00'),
+      }
+    ];
+  }
+
   toggle() {
     this.hideRooms = !this.hideRooms;
   }
