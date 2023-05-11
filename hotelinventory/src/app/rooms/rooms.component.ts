a0a166867ca2ee89af09b752952fd3e3bbe9436f
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-05-11 09:25:51 +0530

    Services
    How to get data from services using dependency injection

--- hotelinventory/src/app/rooms/rooms.component.ts
+++ hotelinventory/src/app/rooms/rooms.component.ts
@@ -1,6 +1,7 @@
 import { Component, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { Room, RoomList } from './rooms';
 import { HeaderComponent } from '../header/header.component';
+import { RoomsService } from './services/rooms.service';
 
 @Component({
   selector: 'hotelinventory-rooms',
@@ -17,48 +18,24 @@ export class RoomsComponent {
     availableRooms: 10,
   }
   title = 'Room List';
-  hideRooms = false;
+  hideRooms = true;
 
   roomList: RoomList[] = [];
+  selectedRoom!: RoomList;
 
   // @ViewChild(HeaderComponent, {static: true}) headerComponent!: HeaderComponent;
   // @ViewChild(HeaderComponent) headerComponent!: HeaderComponent; //recomended way
 
   @ViewChildren(HeaderComponent) headerChildrenComponent!: QueryList<HeaderComponent>;
 
-  selectedRoom!: RoomList;
-  constructor() { }
+  // roomService = new RoomsService(); this is how we initialize a class but to use dependency injection we should not do it, rather do this
+  //always make services private
+  //this instance will not leak into the template or anywhere else but ts file
+
+  constructor(private roomsService: RoomsService) { }
 
   ngOnInit(): void {
-    this.roomList = [
-      {
-        roomNumber: 101,
-        roomType: 'Deluxe',
-        amenities: ['TV', 'AC', 'Wifi'],
-        price: 2000,
-        photos: 'https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-        checkinTime: new Date('2020-01-01T10:00:00'),
-        checkoutTime: new Date('2020-01-01T12:00:00')
-      },
-      {
-        roomNumber: 102,
-        roomType: 'Standard',
-        amenities: ['TV', 'AC'],
-        price: 1500,
-        photos: 'https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-        checkinTime: new Date('2020-01-01T10:00:00'),
-        checkoutTime: new Date('2020-01-01T12:00:00'),
-      },
-      {
-        roomNumber: 103,
-        roomType: 'Economy',
-        amenities: ['TV'],
-        price: 1000,
-        photos: 'https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-        checkinTime: new Date('2020-01-01T10:00:00'),
-        checkoutTime: new Date('2020-01-01T12:00:00'),
-      }
-    ];
+    this.roomList = this.roomsService.getRooms();
   }
 
   toggle() {
---
+++ hotelinventory/src/app/rooms/services/rooms.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { RoomList } from '../rooms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoomsService {
+
+  constructor() { }
+
+  roomList : RoomList[] = [
+    {
+      roomNumber: 101,
+      roomType: 'Deluxe',
+      amenities: ['TV', 'AC', 'Wifi'],
+      price: 2000,
+      photos: 'https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      checkinTime: new Date('2020-01-01T10:00:00'),
+      checkoutTime: new Date('2020-01-01T12:00:00')
+    },
+    {
+      roomNumber: 102,
+      roomType: 'Standard',
+      amenities: ['TV', 'AC'],
+      price: 1500,
+      photos: 'https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      checkinTime: new Date('2020-01-01T10:00:00'),
+      checkoutTime: new Date('2020-01-01T12:00:00'),
+    },
+    {
+      roomNumber: 103,
+      roomType: 'Economy',
+      amenities: ['TV'],
+      price: 1000,
+      photos: 'https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      checkinTime: new Date('2020-01-01T10:00:00'),
+      checkoutTime: new Date('2020-01-01T12:00:00'),
+    }
+  ];
+
+  getRooms() {
+    return this.roomList;
+  }
+
+}
