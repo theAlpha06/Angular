9da21bdfc67b72a69dbdd5bafba26c6fb1f237b0
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-05-11 10:08:50 +0530

    @Host() decorator make different instance of the service in the container

--- hotelinventory/src/app/container/container.component.ts
+++ hotelinventory/src/app/container/container.component.ts
@@ -1,14 +1,20 @@
-import { Component, ContentChild } from '@angular/core';
+import { Component, ContentChild, Host } from '@angular/core';
 import { EmployeeComponent } from '../employee/employee.component';
+import { RoomsService } from '../rooms/services/rooms.service';
 
 @Component({
   selector: 'hotelinventory-container',
   templateUrl: './container.component.html',
-  styleUrls: ['./container.component.scss']
+  styleUrls: ['./container.component.scss'],
+  providers: [RoomsService]
 })
 export class ContainerComponent {
   @ContentChild(EmployeeComponent) employee!: EmployeeComponent;
+  constructor(@Host() private roomsService: RoomsService) {
+
+  }
   ngAfterContentInit(): void {
     this.employee.employee = 'Singh';
   }
+
 }
