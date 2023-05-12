4cd35175cb97ee3ce23e7bc918d2b01fc0853277
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-05-12 12:01:07 +0530

    put

--- hotelinventory/src/app/rooms/rooms.component.ts
+++ hotelinventory/src/app/rooms/rooms.component.ts
@@ -98,6 +98,21 @@ export class RoomsComponent {
     })
   }
 
+  editRoom() {
+    const room: RoomList = {
+      roomNumber: '3',
+      roomType: 'Economy',
+      amenities: ['TV'],
+      price: 1000,
+      photos: 'https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      checkinTime: new Date('2020-01-01T10:00:00'),
+      checkoutTime: new Date('2020-01-01T12:00:00')
+    }
+    this.roomsService.editRoom(room).subscribe((data) => {
+      this.roomList = data;
+    })
+  }
+
 }
 
 //do not implement ngOnChanges and ngDoCheck on same component
--- hotelinventory/src/app/rooms/services/rooms.service.ts
+++ hotelinventory/src/app/rooms/services/rooms.service.ts
@@ -30,4 +30,7 @@ export class RoomsService {
     return this.http.post<RoomList[]>('/api/rooms', room);
   }
 
+  editRoom(room: RoomList) {
+    return this.http.put<RoomList[]>(`/api/rooms/${room.roomNumber}`, room);
+  }
 }
