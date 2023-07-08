7fc15c6a2b849ee927323d55e09855209a11da5f
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-07-08 12:40:16 +0530

    ngClass

--- Introduction/src/app/server/server.component.ts
+++ Introduction/src/app/server/server.component.ts
@@ -3,7 +3,11 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styles: [],
+  styles: [`
+    .online {
+      color: white;
+    }
+  `],
 })
 export class ServerComponent {
   serverId = 10;
