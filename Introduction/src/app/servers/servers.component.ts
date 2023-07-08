6db4154f44f1d28626a162102f5da28be80b33b4
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-07-08 11:24:25 +0530

    Event binding

--- Introduction/src/app/servers/servers.component.ts
+++ Introduction/src/app/servers/servers.component.ts
@@ -8,10 +8,15 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created';
 
   constructor () {
     setTimeout(() => {
       this.allowNewServer = true;
-    }, 10000);
+    }, 2000);
+  }
+
+  onCreateServer () {
+    this.serverCreationStatus = 'Server was created.'
   }
 }
