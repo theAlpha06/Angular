267718ce20b5b3aee5a183df175040907d96bf33
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-07-08 12:52:06 +0530

    ngFor

--- Introduction/src/app/servers/servers.component.ts
+++ Introduction/src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent {
   serverCreationStatus = 'No server was created';
   serverName = '';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor () {
     setTimeout(() => {
@@ -20,6 +21,7 @@ export class ServersComponent {
 
   onCreateServer () {
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.servers.push(this.serverName);
     this.serverCreated = true;
   }
   // onUpdateServerName(event: Event) {
