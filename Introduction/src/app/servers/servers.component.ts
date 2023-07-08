23b4ef07cc2c7e677bf612e3eda5fdce7cd75434
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-07-08 11:31:44 +0530

    More on event binding

--- Introduction/src/app/servers/servers.component.ts
+++ Introduction/src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
+  serverName = '';
 
   constructor () {
     setTimeout(() => {
@@ -18,5 +19,8 @@ export class ServersComponent {
 
   onCreateServer () {
     this.serverCreationStatus = 'Server was created.'
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
