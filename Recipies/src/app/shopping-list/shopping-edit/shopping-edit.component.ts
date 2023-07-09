9985bafd5640f2a68701e516e41797f4f671f3be
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-07-09 11:26:18 +0530

    shopping list works

--- Recipies/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Recipies/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+  onAddItem () {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.nameInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
