df20b8e8fd1181aa949d0177fc478050a6ea6080
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-07-19 15:33:28 +0530

    fix: dropdown

--- Recipies/src/app/shared/dropdown.directive.ts
+++ Recipies/src/app/shared/dropdown.directive.ts
@@ -4,13 +4,10 @@ import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core'
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  // @HostBinding('class.open') isOpen = false;
-  // @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
-  //   this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
-  // }
-  //   constructor(private elRef: ElementRef) { }
-  @HostListener('click') toggleOpen() {
-   console.log('click')
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
+    constructor(private elRef: ElementRef) { }
 
 }
